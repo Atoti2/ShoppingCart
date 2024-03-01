68228dfd1f0bb3c66c54ca8a417e0a6cc69ccb2b
Atoti2 <bogyikormi3@gmail.com>
2024-03-01 17:14:54 +0100

    cart animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,46 @@ import NotFound from "./pages/NotFound.jsx"
 import Details from "./components/Details.jsx"
 import { useState } from "react"
 import Cart from "./components/Cart.jsx"
+import { AnimatePresence } from "framer-motion"
 
 function App() {
   const [cart, setCart] = useState([])
   const [open, setOpen] = useState(false)
 
   function handleAddToCart(newProduct) {
-        let productsToAdd = []
-        productsToAdd.push(newProduct)
-        setCart([...cart, ...productsToAdd])
+    const index = cart.findIndex((item) => item.id === newProduct.id);
+    if (index === -1) {
+        setCart([...cart, newProduct]);
+
+    } else {
+        const updatedCart = [...cart];
+        updatedCart[index] = {
+            ...updatedCart[index],
+            quantity: updatedCart[index].quantity + newProduct.quantity
+        };
+        setCart(updatedCart);
+    }
 }
 
   return (
     <>
-      <RootLayout open={open} setOpen={setOpen}/>
+    <div onClick={() => setOpen(!open)} className={open ? "fixed w-full h-full top-0 left-0 right-0 bottom-0 z-10 bg-black/30 cursor-pointer " : ""}></div>
+    <RootLayout open={open} setOpen={setOpen}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="shop" element={<Shop handleAddToCart={handleAddToCart}/>}/>
         <Route path="shop/:id" element={<Details/>}/> 
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
+    
       
-      <div className="fixed right-0 top-0 rounded-md overflow-hidden min-h-screen">
-        {open && <Cart open={open} setOpen={setOpen} cart={cart}/>}
+      <div className="fixed right-0 top-0  z-20 rounded-md overflow-hidden min-h-screen">
+      <AnimatePresence>
+        {open && 
+          <Cart
+           open={open} setOpen={setOpen} cart={cart} 
+        />}
+      </AnimatePresence>
       </div>
   
     </>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,31 @@
 import { motion } from "framer-motion"
 const Cart = ({open, setOpen, cart}) => {
-    console.log(cart);
+    const total = cart.reduce((total, item) => total + (item.price * item.quantity), 0);
     return(
         <>
         <motion.div
-        initial={{translateX: 50}}
+        initial={{translateX: 384}}
         animate={{translateX: 0}}
-        exit={{translateX: 50}}
-         className="bg-white min-h-screen w-64 -z-10">
-            <span className="text-black text-2xl font-bold block" onClick={() => {setOpen(!open)}}><svg className="w-10 h-10 cursor-pointer ml-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg></span>
+        exit={{translateX: 384}}
+        transition={{type: "just"}}
+         className="bg-white min-h-screen w-96 flex flex-col">
+            <span className="text-black text-2xl font-bold block" onClick={() => {setOpen(!open)}}><svg className="w-10 h-10 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg></span>
             {cart.map((item) => {
                 return(
-                    <h1 key={item.id}>{item.title}</h1>
+                    <motion.div 
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    key={item.id} className="text-slate-900 m-5">
+                        <h1 className="text-xl font-bold">{item.title}</h1>
+                        <p className="text-lg italic">${item.price}</p>
+                        <p>{item.quantity} pcs</p>
+                    </motion.div>
                 )
             })}
+                <p className="text-xl mt-auto m-5 text-black">{total > 0 ? `Total: $${total}`  : ""}</p>
+           
+           
         </motion.div>
          
            
