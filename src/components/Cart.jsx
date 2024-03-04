7132fb036724a2c8494493b36274536362703fa4
Atoti2 <bogyikormi3@gmail.com>
2024-03-04 16:45:36 +0100

    cart scroll bug fix

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,7 +9,7 @@ const Cart = ({open, setOpen, cart, removeFromCart}) => {
         animate={{translateX: 0}}
         exit={{translateX: 384}}
         transition={{type: "just"}}
-        className="bg-white min-h-screen w-64 sm:w-96 flex flex-col">
+        className="bg-white h-screen overflow-y-scroll w-64 sm:w-96 flex flex-col">
             <span className="text-slate-900 text-2xl font-bold block m-5" onClick={() => {setOpen(!open)}}>
                 <svg className="w-10 h-10 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>
             </span>
