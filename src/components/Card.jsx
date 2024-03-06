3010c57c6e92fa3588a0e11f284068fa2cb6cc0b
Atoti2 <bogyikormi3@gmail.com>
2024-03-06 19:53:37 +0100

    animation fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,6 +19,29 @@ const Card = ({id, image, price, title, handleAdd}) => {
         }
     }
 
+    function startAnimate(){
+        return(
+        <>
+            <motion.p
+            initial={{left: `{${position.x}px`, top:`${position.y}px` }}
+            animate={{
+                top: 15, 
+                right: 30,
+                scale: 1.5,
+                transitionEnd: {
+                    display: "none",
+                    top: 0,
+                    left: 0,
+                }
+            }}
+            transition={{type: "just", duration: 0.3}}
+            className="text-3xl text-black font-bold inline fixed">{quantity}
+            </motion.p>
+            </>
+        )
+        
+    }
+
 
     return(
         <>
@@ -32,18 +55,21 @@ const Card = ({id, image, price, title, handleAdd}) => {
                     <h1 className="text-xl text-black">{title}</h1>
                     <p className="text-lg italic text-black">${price}</p>
 
-                        <motion.button 
+                        <button 
 
                         onClick={((e) => {
                             handleAdd({id, title, price, quantity})
-                            setAnimating(!animating)
+                            setAnimating(true)
                             setPosition({
                                 x: e.clientX,
                                 y: e.clientY
                             })
-                            
+                            setTimeout(() => {
+                                setAnimating(false)
+                            }, 300)
+                          
                         })} 
-                        className="bg-black text-white p-5 mt-5 rounded-md hover:bg-black/80 transition-all">Add to cart</motion.button>
+                        className="bg-black text-white p-5 mt-5 rounded-md hover:bg-black/80 transition-all">Add to cart</button>
                     
                         <div className="flex mt-5 gap-3 items-center">
                             <p className="text-xl text-white bg-black rounded-md hover:bg-black/80 cursor-pointer p-2 w-8 text-center" 
@@ -64,7 +90,8 @@ const Card = ({id, image, price, title, handleAdd}) => {
                                     }
                                 }}
                                 transition={{type: "just", duration: 0.3}}
-                                className="text-3xl text-black font-bold inline fixed">{quantity}</motion.p>
+                                className="text-3xl text-black font-bold inline fixed">{quantity}
+                                </motion.p>
                                 </>
                                 )   
                                 }
